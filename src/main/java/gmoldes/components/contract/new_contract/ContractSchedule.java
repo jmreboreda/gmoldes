b47426c3e09e4db607b816ba6adc6f72ed2844da
jmreboreda <jmreboreda@mundo-r.com>
2018-03-06 02:50:50 +0100

    TableView editing string cellOk.

--- src/main/java/gmoldes/components/contract/new_contract/ContractSchedule.java
+++ src/main/java/gmoldes/components/contract/new_contract/ContractSchedule.java
@@ -1,33 +1,49 @@
 package gmoldes.components.contract.new_contract;
 
 import gmoldes.components.ViewLoader;
+import gmoldes.domain.dto.ContractScheduleDayDTO;
+import gmoldes.utilities.EditingStringCell;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContractSchedule extends AnchorPane {
 
-    private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("dd-MM-yyyy");
-    private static final SimpleDateFormat TIME_FORMATTER = new SimpleDateFormat("HH:mm");
+    private static final String SCHEDULE_FXML = "/fxml/new_contract/contract_schedule_table.fxml";
+
     private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     private DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-
-    private String date = LocalDate.now().format(dateFormatter);
-
-    private static final String SCHEDULE_FXML = "/fxml/new_contract/contract_schedule.fxml";
 
     private Parent parent;
 
     @FXML
-    private ContractSchedule contractSchedule;
+    private TableView <ContractScheduleDayDTO> contract_schedule_table;
     @FXML
-    private GridPane scheduleBoard;
-
+    private TableColumn<ContractScheduleDayDTO, String> dayOfWeek;
+    @FXML
+    private TableColumn<ContractScheduleDayDTO, LocalDate> date;
+    @FXML
+    private TableColumn<ContractScheduleDayDTO, LocalTime> amFrom;
+    @FXML
+    private TableColumn<ContractScheduleDayDTO, LocalTime> amTo;
+    @FXML
+    private TableColumn<ContractScheduleDayDTO, LocalTime> pmFrom;
+    @FXML
+    private TableColumn<ContractScheduleDayDTO, LocalTime> pmTo;
+    @FXML
+    private TableColumn<ContractScheduleDayDTO, Double> totalDayHours;
 
     public ContractSchedule() {
         this.parent = ViewLoader.load(this, SCHEDULE_FXML);
@@ -36,6 +52,37 @@ public class ContractSchedule extends AnchorPane {
     @FXML
     public void initialize() {
 
-    }
+        contract_schedule_table.setEditable(true);
 
+        Callback<TableColumn<ContractScheduleDayDTO, String>, TableCell<ContractScheduleDayDTO, String>> cellFactory =
+                p -> new EditingStringCell();
+        dayOfWeek.setCellFactory(cellFactory);
+
+
+
+        dayOfWeek.setCellValueFactory(new PropertyValueFactory<>("dayOfWeek"));
+        date.setCellValueFactory(new PropertyValueFactory<>("date"));
+        amFrom.setCellValueFactory(new PropertyValueFactory<>("amFrom"));
+        amTo.setCellValueFactory(new PropertyValueFactory<>("amTo"));
+        pmFrom.setCellValueFactory(new PropertyValueFactory<>("pmFrom"));
+        pmTo.setCellValueFactory(new PropertyValueFactory<>("pmTo"));
+        totalDayHours.setCellValueFactory(new PropertyValueFactory<>("totalDayHours"));
+        date.setStyle("-fx-alignment: CENTER;");
+        amFrom.setStyle("-fx-alignment: CENTER;");
+        amTo.setStyle("-fx-alignment: CENTER;");
+        pmFrom.setStyle("-fx-alignment: CENTER;");
+        pmTo.setStyle("-fx-alignment: CENTER;");
+        totalDayHours.setStyle("-fx-alignment: CENTER-RIGHT;");
+
+        List<ContractScheduleDayDTO> contractScheduleDayDTOList = new ArrayList<>();
+        for(int i = 0; i < 11; i++){
+            contractScheduleDayDTOList.add(
+                    new ContractScheduleDayDTO("", null, LocalTime.MIN, LocalTime.MIN, LocalTime.MIN, LocalTime.MIN, 0.00D));
+        }
+
+        ObservableList<ContractScheduleDayDTO> data = FXCollections.observableArrayList(contractScheduleDayDTOList);
+        contract_schedule_table.setItems(data);
+
+
+    }
 }
