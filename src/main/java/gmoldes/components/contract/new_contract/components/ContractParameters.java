d9afdb4b181aee91b1cf7f6e08254e651201b610
jmreboreda <jmreboreda@mundo-r.com>
2018-12-11 03:07:27 +0100

    Record of contract extension, in development.

--- src/main/java/gmoldes/components/contract/new_contract/components/ContractParameters.java
+++ src/main/java/gmoldes/components/contract/new_contract/components/ContractParameters.java
@@ -4,6 +4,8 @@ public class ContractParameters {
 
     public static final Integer MAXIMUM_NUMBER_OF_EXTENSIONS_OF_A_CONTRACT = 1;
     public static final Long NUMBER_MONTHS_MAXIMUM_DURATION_INITIAL_CONTRACT_PLUS_ITS_EXTENSIONS = 12L;
-    public static final Double AVERAGE_OF_DAYS_IN_A_MONTH = 30.4375;  // (3 years plus a leap year)/ (48 months in four years) = 30.4375
+
+    // (The sum of the days of three normal years plus those of a leap year divided by the 48 months of the four years = 30.4375 days of average
+    public static final Double AVERAGE_OF_DAYS_IN_A_MONTH = (3.0 * 365.0 + 366.0)/48.0;
 
 }
