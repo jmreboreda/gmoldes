08b9be51e34bf0760b146aa082192f9d9e32e7c5
jmreboreda <jmreboreda@mundo-r.com>
2019-05-04 23:21:01 +0200

    Changes in ApplicationMainController and ApplicationMainManager.

--- src/main/java/gmoldes/ApplicationMainManager.java
+++ src/main/java/gmoldes/ApplicationMainManager.java
@@ -8,29 +8,11 @@ import gmoldes.domain.contract.dto.ContractVariationDTO;
 import gmoldes.domain.contract.dto.TypesContractVariationsDTO;
 import gmoldes.domain.contract.mapper.MapperContractVariationVODTO;
 import gmoldes.domain.contract.mapper.MapperTypesContractVariationsVODTO;
-import gmoldes.domain.traceability_contract_documentation.dto.TraceabilityContractDocumentationDTO;
-import gmoldes.domain.traceability_contract_documentation.mapper.MapperTraceabilityContractDocumentationVODTO;
-import gmoldes.domain.traceability_contract_documentation.persistence.dao.TraceabilityContractDocumentationDAO;
-import gmoldes.domain.traceability_contract_documentation.persistence.vo.TraceabilityContractDocumentationVO;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ApplicationMainManager {
-
-    public List<TraceabilityContractDocumentationDTO> findTraceabilityForAllContractWithWorkingDayScheduleWithEndDate(){
-
-        List<TraceabilityContractDocumentationDTO> traceabilityContractDocumentationDTOList = new ArrayList<>();
-
-        TraceabilityContractDocumentationDAO traceabilityContractDocumentationDAO = TraceabilityContractDocumentationDAO.TraceabilityContractDocumentationDAOFactory.getInstance();
-        List<TraceabilityContractDocumentationVO> traceabilityContractDocumentationVOList = traceabilityContractDocumentationDAO.findTraceabilityForAllContractWithWorkingDayScheduleWithEndDate();
-
-        for(TraceabilityContractDocumentationVO traceabilityContractDocumentationVO : traceabilityContractDocumentationVOList){
-            traceabilityContractDocumentationDTOList.add(MapperTraceabilityContractDocumentationVODTO.map(traceabilityContractDocumentationVO));
-        }
-
-        return traceabilityContractDocumentationDTOList;
-    }
 
     public TypesContractVariationsDTO retrieveTypesContractVariations(Integer variationType){
         TypesContractVariationsDAO typesContractVariationsDAO = TypesContractVariationsDAO.TypesContractVariationsDAOFactory.getInstance();
