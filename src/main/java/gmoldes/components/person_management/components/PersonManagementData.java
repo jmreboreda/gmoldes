afab2888c8e87c975b158346cda4074add516d7e
jmreboreda <jmreboreda@mundo-r.com>
2019-05-16 14:19:33 +0200

    New Person. Interface development.

--- src/main/java/gmoldes/components/person_management/components/PersonManagementData.java
+++ src/main/java/gmoldes/components/person_management/components/PersonManagementData.java
@@ -29,7 +29,7 @@ public class PersonManagementData extends VBox {
     @FXML
     private TextField Nationality;
     @FXML
-    private TextField personExtendDirection;
+    private TextField personExtendedDirection;
     @FXML
     private TextField personPostalCode;
     @FXML
@@ -39,13 +39,104 @@ public class PersonManagementData extends VBox {
     @FXML
     private ChoiceBox personStudy;
 
-
-
-
-
-
     public PersonManagementData() {
 
         this.parent = ViewLoader.load(this, NEW_PERSON_DATA_FXML);
+    }
+
+    public TextField getPersonSurName() {
+        return personSurName;
+    }
+
+    public void setPersonSurName(TextField personSurName) {
+        this.personSurName = personSurName;
+    }
+
+    public TextField getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(TextField personName) {
+        this.personName = personName;
+    }
+
+    public TextField getPersonNIF() {
+        return personNIF;
+    }
+
+    public void setPersonNIF(TextField personNIF) {
+        this.personNIF = personNIF;
+    }
+
+    public TextField getPersonNASS() {
+        return personNASS;
+    }
+
+    public void setPersonNASS(TextField personNASS) {
+        this.personNASS = personNASS;
+    }
+
+    public DatePicker getPersonBirthDate() {
+        return personBirthDate;
+    }
+
+    public void setPersonBirthDate(DatePicker personBirthDate) {
+        this.personBirthDate = personBirthDate;
+    }
+
+    public TextField getCivilStatus() {
+        return civilStatus;
+    }
+
+    public void setCivilStatus(TextField civilStatus) {
+        this.civilStatus = civilStatus;
+    }
+
+    public TextField getNationality() {
+        return Nationality;
+    }
+
+    public void setNationality(TextField nationality) {
+        Nationality = nationality;
+    }
+
+    public TextField getPersonExtendedDirection() {
+        return personExtendedDirection;
+    }
+
+    public void setPersonExtendDirection(TextField personExtendDirection) {
+        this.personExtendedDirection = personExtendDirection;
+    }
+
+    public TextField getPersonPostalCode() {
+        return personPostalCode;
+    }
+
+    public void setPersonPostalCode(TextField personPostalCode) {
+        this.personPostalCode = personPostalCode;
+    }
+
+    public TextField getPersonLocation() {
+        return personLocation;
+    }
+
+    public void setPersonLocation(TextField personLocation) {
+        this.personLocation = personLocation;
+    }
+
+    public TextField getPersonMunicipality() {
+        return personMunicipality;
+    }
+
+    public void setPersonMunicipality(TextField personMunicipality) {
+        this.personMunicipality = personMunicipality;
+    }
+
+    public ChoiceBox getPersonStudy() {
+        return personStudy;
+    }
+
+    public void setPersonStudy(ChoiceBox personStudy) {
+        this.personStudy = personStudy;
     }
 }
