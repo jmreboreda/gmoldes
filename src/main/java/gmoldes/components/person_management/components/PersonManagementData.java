e28a03c38a912f03e9555d3d8ead26a53ac5c900
jmreboreda <jmreboreda@mundo-r.com>
2019-05-18 22:54:24 +0200

    New Person. Interface development.

--- src/main/java/gmoldes/components/person_management/components/PersonManagementData.java
+++ src/main/java/gmoldes/components/person_management/components/PersonManagementData.java
@@ -74,104 +74,52 @@ public class PersonManagementData extends VBox {
         return personSurName;
     }
 
-    public void setPersonSurName(TextField personSurName) {
-        this.personSurName = personSurName;
-    }
-
     public ComboBox getPersonSurNames() {
         return personSurNames;
-    }
-
-    public void setPersonSurNames(ComboBox personSurNames) {
-        this.personSurNames = personSurNames;
     }
 
     public TextField getPersonName() {
         return personName;
     }
 
-    public void setPersonName(TextField personName) {
-        this.personName = personName;
-    }
-
     public TextField getPersonNIF() {
         return personNIF;
-    }
-
-    public void setPersonNIF(TextField personNIF) {
-        this.personNIF = personNIF;
     }
 
     public TextField getPersonNASS() {
         return personNASS;
     }
 
-    public void setPersonNASS(TextField personNASS) {
-        this.personNASS = personNASS;
-    }
-
     public DatePicker getPersonBirthDate() {
         return personBirthDate;
-    }
-
-    public void setPersonBirthDate(DatePicker personBirthDate) {
-        this.personBirthDate = personBirthDate;
     }
 
     public TextField getCivilStatus() {
         return civilStatus;
     }
 
-    public void setCivilStatus(TextField civilStatus) {
-        this.civilStatus = civilStatus;
-    }
-
     public TextField getNationality() {
         return Nationality;
-    }
-
-    public void setNationality(TextField nationality) {
-        Nationality = nationality;
     }
 
     public TextField getPersonExtendedDirection() {
         return personExtendedDirection;
     }
 
-    public void setPersonExtendDirection(TextField personExtendDirection) {
-        this.personExtendedDirection = personExtendDirection;
-    }
-
     public TextField getPersonPostalCode() {
         return personPostalCode;
-    }
-
-    public void setPersonPostalCode(TextField personPostalCode) {
-        this.personPostalCode = personPostalCode;
     }
 
     public TextField getPersonLocation() {
         return personLocation;
     }
 
-    public void setPersonLocation(TextField personLocation) {
-        this.personLocation = personLocation;
-    }
-
     public TextField getPersonMunicipality() {
         return personMunicipality;
     }
 
-    public void setPersonMunicipality(TextField personMunicipality) {
-        this.personMunicipality = personMunicipality;
-    }
-
     public ChoiceBox getPersonStudy() {
         return personStudy;
-    }
-
-    public void setPersonStudy(ChoiceBox personStudy) {
-        this.personStudy = personStudy;
     }
 
     private void loadStudy(){
