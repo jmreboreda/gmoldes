5e17bcb588984c6b1f3449bce95db9c03fc09240
RBJM <jmreboreda@mundo-r.com>
2019-06-06 12:16:18 +0200

    Various changes in Payroll Check List.

--- src/main/java/gmoldes/domain/check/CheckConstants.java
+++ src/main/java/gmoldes/domain/check/CheckConstants.java
@@ -3,6 +3,7 @@ package gmoldes.domain.check;
 public class CheckConstants {
 
     public static final String INITIAL_CHECK_HEADER_TEXT = "Controles iniciales - ";
+    public static final String FINALIZED_CHECK_APPLICATION_AGENDA ="Ha finalizado la comprobación de la agenda de la aplicación.\n\n";
     public static final Integer END_OF_CONTRACT_NOTICE_DAYS = 20;
     public static final Integer LIMIT_DAYS_DELAY_RECEIPT_CONTRACT_LABOR_DOCUMENTATION = 5;
 }
